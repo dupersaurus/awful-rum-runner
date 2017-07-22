535b26acd87aee0294b941b5e1bc622e247f922d
Jason DuPertuis <gallahad@me.com>
2017-07-22 18:09:17 -0400

    Place some AI

--- Assets/Scripts/AI/PatrolState.cs
+++ Assets/Scripts/AI/PatrolState.cs
@@ -23,11 +23,37 @@ public class PatrolState : AIState {
 
 	void OnDrawGizmos() {
 		Gizmos.color = Color.white;
+		Vector3 lastPos = _waypoints[_waypoints.Count - 1];
+		lastPos.y = 1;
 
 		for (int i = 0; i < _waypoints.Count; i++) {
 			Vector3 pos = _waypoints[i];
 			pos.y = 1;
+
 			Gizmos.DrawWireSphere(pos, 0.3f);
+			Gizmos.DrawLine(lastPos, pos);
+
+			lastPos = pos;
+		}
+
+		Gizmos.color = Color.magenta;
+		Vector3 start = _waypoints[0];
+		Gizmos.DrawLine(transform.position + Vector3.up, start + Vector3.up);
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.yellow;
+		Vector3 lastPos = _waypoints[_waypoints.Count - 1];
+		lastPos.y = 1;
+
+		for (int i = 0; i < _waypoints.Count; i++) {
+			Vector3 pos = _waypoints[i];
+			pos.y = 1;
+
+			Gizmos.DrawWireSphere(pos, 0.3f);
+			Gizmos.DrawLine(lastPos, pos);
+
+			lastPos = pos;
 		}
 	}
 
